ffb5e9bd32faa2a041f945d7ebc45eb30857cc67
Ourania Ntrizi <drizirania@gmail.com>
2025-02-16 02:43:58 +0200

    fixes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -8,13 +8,21 @@ function About() {
       <h2>About Me</h2>
       <p>
         I'm a junior React developer who loves building modern web apps. I specialize in
-        creating clean and interactive UI using React. My skills include:
+        creating clean and interactive UI using React.
       </p>
+      <h3>
+         My skills include:
+      </h3>
       <ul>
         <li>React & Redux</li>
         <li>HTML5 & CSS3</li>
         <li>JavaScript (ES6+)</li>
         <li>REST APIs & AJAX</li>
+        <li>Firebase</li>
+        <li>Docker</li>
+        <li>SQL</li>
+        <li>C</li>
+        <li>Python</li>
       </ul>
     </section>
   );
